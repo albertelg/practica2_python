f2493fedc7740566d26088280254159ed34967ab
jordipermanyer <jordi.permanyer@uvic.cat>
2022-01-20 10:59:06 +0100

    a

--- practica2.py
+++ practica2.py
@@ -1,6 +1,14 @@
 #Practica 2; Jordi Permanyer i Albert Elgstrom
 import random
-def obtenirJugadors():
+
+def creaDic(nomJ):
+    
+    pass
+def obtenirJugadors(f):
+    d={} #nomJug:[mitj,gunyat,perdut]
+    
+    
+    return d
     pass
 def partides():
     pass
@@ -13,7 +21,8 @@ def joc(taulell,jugador,aleatori_random):
     pass
 
 if __name__=="__main__":
-    dJugadors=obtenirJugadors('jugadors.csv')
+    file=open("jugadors","w")
+    dJugadors=obtenirJugadors(file)
     partides(dJugadors)
     guardarEstat(dJugadors)
     mostrarRanking(dJugadors)
